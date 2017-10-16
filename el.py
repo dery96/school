a05002a0d4d8e49acfefdf156ad325fba3aa6879
Dominik Szyja <dery96@github.com>
2017-10-16 18:41:32 +0200

    MazeSolve Class recursiveSolve implemention writing

--- el.py
+++ el.py
@@ -1,5 +1,9 @@
-    def getBeginingPos(self):
-        for i in len(self.mazeMap):
-            try:
-                return self.mazeMap[i].index('@')
-            
+mazeMap = [[1, 2, 3], ['@'], ['a']]
+def getBeginingPos(mazeMap):
+    for i in mazeMap:
+        try:
+            print i.index('@')
+        except:
+            pass
+
+getBeginingPos(mazeMap)
--- start.py
+++ start.py
@@ -1,67 +1,57 @@
 class MazeSolve(object):
     """docstring for MazeSolve."""
-    def __init__(self, mazeMap):
-        super(MazeSolve, self).__init__()
+    def __init__(self, mazeMap, width, height):
         self.mazeMap = mazeMap
-        self.end = "It's end \n %s" % (self.mazeMap)
-        self.pos = self.getBeginingPos()
-        self.actualPositionContent = self.mazeMap[self.pos['x']][self.pos['y']]
         self.width = width
         self.height = height
+        # self.actualPositionContent = self.mazeMap[self.pos[0]][self.pos[1]]
+        self.end = "It's end \n %s" % (self.mazeMap)
+        self.posX, self.posY = self.getBeginingPos()[0], self.getBeginingPos[1]
 
+    def checkIfEnd(self):
+        if self.Maze[self.pos[0]][self.pos[1]] == '$':
+            return true
+        return false
 
-    def solveRecursive(self):
-        print(self.posX, self.posY)
-        if self.checkIfEnd:
-            return self.end
-        else:
-            if self.actualPositionContent == ' ':
-
-
-            if self.actualPositionContent == '#':
-                pass
-
-
-
-    def getCloserDirection(self):
-
-        pass
-
-    def chooseDirection(self):
-        if
+    def testPos(x, y):
+        if y < width and x < height:
+            return True
+        return False
 
     def getBeginingPos(self):
         for i in len(self.mazeMap):
             try:
-                return self.mazeMap[i].index('@')
-        # return self.mazeMap.index('@')
+                return i, self.mazeMap[i].index('@')
+            except:
+                pass
 
-    def checkIfEnd(self):
-        if self.Maze[self.posY][self.posX] == '$':
-            return true
-        return false
-
+    def solveRecursive(self, x=self.posX, y=self.posY):
+        print("(%d, %d)" % (x, y))
+        if self.checkIfEnd():
+            return self.end
+        else:
+            if self.testPos(x+1,y) and self.mazeMap[x+1][y] !== '#':
+                # Up
+                self.mazeMap[x+1, y] = '.'
+                return self.solveRecursive(x+1, y)
+            elif self.testPos(x,y+1) and self.mazeMap[x][y+1] !== '#':
+                # Right
+                self.mazeMap[x, y+1] = '.'
+                return self.solveRecursive(x, y+1)
+            elif self.testPos(x-1,y) and self.mazeMap[x-1][y] !== '#':
+                # Down
+                self.mazeMap[x-1, y] = '.'
+                return self.solveRecursive(x-1,y)
+            elif self.testPos(x,y-1) and self.mazeMap[x][y-1] !== '#':
+                # Left
+                self.mazeMap[x, y-1] = '.'
+                return self.solveRecursive(x, y-1)
 
 def mazeRekurencyjnie(mazeWidth, mazeHeight):
     pass
 
 ## Maybe it's time to use Dickstra
 ## między dwoma punktami zawsze ma być droga
-
-def moveUp(arg):
-    pass
-
-def moveRight(arg):
-    pass
-
-def moveDown(arg):
-    pass
-
-def moveLeft(arg):
-    pass
-
-def move(arg):
-    pass
 
 def analzyeMapToGraph(arg):
     ## it sets weight for map
