ea57f93d790523f22cd19b063c1041397103eb52
dery96 <dery1996@gmail.com>
2017-10-16 23:37:17 +0200

    Clean Master

--- dijkstra_algorithm.py
+++
@@ -1,5 +0,0 @@
-class Dijkstra(object):
-    """docstring for Dijkstra."""
-    def __init__(self, arg):
-        super(Dijkstra, self).__init__()
-        self.arg = arg
--- el.py
+++
@@ -1,9 +0,0 @@
-mazeMap = [[1, 2, 3], ['@'], ['a']]
-def getBeginingPos(mazeMap):
-    for i in mazeMap:
-        try:
-            print i.index('@')
-        except:
-            pass
-
-getBeginingPos(mazeMap)
