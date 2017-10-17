6705f1fe41b74b948799d4a17c746dda6e78a90b
Dominik Szyja <dery96@github.com>
2017-10-17 20:40:24 +0200

    solvemaze class polishing

--- Maze.py
+++ Maze.py
@@ -14,8 +14,8 @@ class SolveMaze(object):
     def checkIfEnd(self, x, y):
         return True if self.mazeMap[x][y] == '$' else False
 
-    def testPos(self, x, y):
-        return True if y < self.width - 1 and x < self.height - 1 else False
+    def visited(self, x, y):
+        return True if self.mazeMap[x][y] == '.' else False
 
     def getBeginingPos(self):
         for i in range(len(self.mazeMap)):
@@ -24,36 +24,35 @@ class SolveMaze(object):
             except:
                 pass
 
-    def solveRecursive(self, x, y):
+    def solveRecursive(self, x, y, mazeMap):
+        checkIfEnd = True if mazeMap[x][y] == '$' else False
         print("(%d, %d)" % (x, y))
-        if self.checkIfEnd(x, y):
+        if checkIfEnd:
+            print("KONIEC")
             return self.end
         else:
-            if self.mazeMap[x][y] != '#':
-                # firstly Will go every nearest possibility:
-                # up -> right -> down -> left
-                if self.testPos(x + 1, y) and self.mazeMap[x + 1][y] != '#':
-                    print("UP")
-                    self.posX, self.posY = x + 1, y
-                    self.mazeMap[x + 1][y] = "."
-                    # return self.solveRecursive(x + 1, y)
-                elif self.testPos(x, y + 1) and self.mazeMap[x][y + 1] != '#':
-                    print("RIGHT")
-                    self.posX, self.posY = x, y + 1
-                    self.mazeMap[x][y + 1] = '.'
-                    # return self.solveRecursive(x, y + 1)
-                elif self.testPos(x - 1, y) and self.mazeMap[x - 1][y] != '#':
-                    print("DOWN")
-                    self.posX, self.posY = x - 1, y
-                    self.mazeMap[x - 1][y] = '.'
-                    # return self.solveRecursive(x - 1, y)
-                elif self.testPos(x, y - 1) and self.mazeMap[x][y - 1] != '#':
-                    print("LEFT")
-                    self.posX, self.posY = x, y - 1
-                    self.mazeMap[x][y - 1] = '.'
-                    # return self.solveRecursive(x, y - 1)
-                else:
-                    pass
+            # firstly Will go every nearest possibility:
+            # up -> right -> down -> left
+            visited = True if mazeMap[x][y] == '.' else False
+            mazeMap[x][y] = '.' if mazeMap[x][y] == ' ' else mazeMap[x][y]
+            if not visited and mazeMap[x - 1][y] != '#':
+                print("UP")
+                # self.mazeMap[x + 1][y] = "."
+                return self.solveRecursive(x - 1, y, mazeMap)
+            elif not visited and mazeMap[x][y + 1] != '#':
+                print("RIGHT")
+                # self.mazeMap[x][y + 1] = '.'
+                return self.solveRecursive(x, y + 1, mazeMap)
+            elif not visited and mazeMap[x + 1][y] != '#':
+                print("DOWN")
+                # self.posX, self.posY = x - 1, y
+                # self.mazeMap[x - 1][y] = '.'
+                return self.solveRecursive(x + 1, y, mazeMap)
+            elif not self.visited(x, y - 1) and mazeMap[x][y - 1] != '#':
+                print("LEFT")
+                # self.posX, self.posY = x, y - 1
+                # self.mazeMap[x][y - 1] = '.'
+                return self.solveRecursive(x, y - 1, mazeMap)
 
     def show(self):
         return [print(''.join(line)) for line in self.mazeMap]
@@ -67,7 +66,10 @@ mapProperties = [ int(x) for x in mapFile[0].split(' ') ]
 mapWidth, mapHeight = mapProperties[1], mapProperties[0]
 a = SolveMaze(mapWidth, mapHeight, mapFile[1:])
 
-for i in range(20):
-    a.solveRecursive(a.posX, a.posY)
+# a.solveRecursive(a.posX, a.posY)
+# for i in range(200):
+a.solveRecursive(a.posX, a.posY, mapFile[1:])
 
 a.show()
+print("Wysokosc: %d, Height: %d" %(mapHeight, mapWidth))
+# print(a.mazeMap[7][13])
